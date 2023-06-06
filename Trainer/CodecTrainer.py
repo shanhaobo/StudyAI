47288cfc08fb2fd13a47013668198d32e5786363
shanhaobo <shanhaobo@gmail.com>
2023-06-06 17:07:44 +0800

    Codec

--- Trainer/CodecTrainer.py
+++ Trainer/CodecTrainer.py
@@ -3,5 +3,17 @@ import torch
 from .MultiNNTrainer import MultiNNTrainer
 
 class CodecTrainer(MultiNNTrainer) :
-    def __init__(self, inLearningRate) -> None:
+    def __init__(self, 
+            inEncoder : torch.nn.Module,
+            inDecoder : torch.nn.Module,
+            inLearningRate
+        ) -> None:
         super().__init__(inLearningRate)
+        self.Encoder = inEncoder
+        self.Decoder = inDecoder
+
+    def _BatchTrain(self, inBatchDatum, inBatchLabel, *inArgs, **inKWArgs) :
+        # get BatchSize
+        nBatchSize = inBatchDatum.size(0)
+        
+        pass
