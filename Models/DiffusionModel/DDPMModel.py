dee02553522b22fb280ead1e83fd866efcf66af0
shanhaobo <shanhaobo@gmail.com>
2023-06-08 14:11:09 +0800

    GITs

--- Models/DiffusionModel/DDPMModel.py
+++ Models/DiffusionModel/DDPMModel.py
@@ -1,9 +1,23 @@
 import torch
 from torch.utils.data import DataLoader
+from torch import nn, einsum
+import torch.nn.functional as F
 
 from datetime import datetime
 
+import math
+from inspect import isfunction
+from functools import partial
+
+from einops import rearrange, reduce
+from einops.layers.torch import Rearrange
+
+from Archiver.BaseArchiver import BaseArchiver
 from Models.BaseModel import BaseModel
+from Trainer.BaseTrainer import BaseTrainer
 
+class DDPMModel(BaseModel) :
+    def __init__(self, inTrainer: BaseTrainer, inArchiver: BaseArchiver):
+        super().__init__(inTrainer, inArchiver)
 
-
+    
---
+++ Models/Moduels/UNet.py
@@ -0,0 +1,107 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# UNet的一大层，包含了两层小的卷积
+class DoubleConv(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(DoubleConv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, 3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_ch, out_ch, 3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+# 定义输入进来的第一层
+class InConv(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(InConv, self).__init__()
+        self.conv = DoubleConv(in_ch, out_ch)
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+# 定义encoder中的向下传播，包括一个maxpool和一大层    
+class Down(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(Down, self).__init__()
+        self.mpconv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_ch, out_ch)
+        )
+
+    def forward(self, x):
+        x = self.mpconv(x)
+        return x
+
+# 定义decoder中的向上传播
+class Up(nn.Module):
+    def __init__(self, in_ch, out_ch, bilinear=True):
+        super(Up, self).__init__()
+        # 定义了self.up的方法
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        else:
+            self.up = nn.ConvTranspose2d(in_ch // 2, in_ch // 2, 2, stride=2) # // 除以的结果向下取整
+
+        self.conv = DoubleConv(in_ch, out_ch)
+
+    def forward(self, x1, x2):  # x2是左侧的输出，x1是上一大层来的输出
+        x1 = self.up(x1)
+
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, (diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2))
+        x = torch.cat([x2, x1], dim=1) # 将两个tensor拼接在一起 dim=1：在通道数（C）上进行拼接
+        x = self.conv(x)
+        return x
+
+# 定义最终的输出
+class OutConv(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_ch, out_ch, 1)
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+class Unet(nn.Module):
+    def __init__(self, in_channels, classes): # in_channels 图片的通道数，1为灰度图，3为彩色图
+        super(Unet, self).__init__()
+        self.n_channels = in_channels
+        self.n_classes = classes
+
+        self.inc = InConv(in_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        self.down4 = Down(512, 512)
+        self.up1 = Up(1024, 256)
+        self.up2 = Up(512, 128)
+        self.up3 = Up(256, 64)
+        self.up4 = Up(128, 64)
+        self.outc = OutConv(64, classes)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        x = self.outc(x)
+
+        return x
