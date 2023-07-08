5b5cf6b00b52b531093d00cc28e982f4202779e5
shanhaobo <shanhaobo@gmail.com>
2023-07-08 17:23:22 +0800

    ema

--- KongMing/Utils/AveragedUtils.py
+++ KongMing/Utils/AveragedUtils.py
@@ -3,23 +3,22 @@ from typing import Any
 
 
 class AveragedUtilBase:
-    def __init__(self, inInitValue) -> None:
+    def __init__(self, inInitValue, inAccpetNewValueFunc) -> None:
         self.AveragedValue = inInitValue
+        self.AcceptNewValue = inAccpetNewValueFunc
 
     def item(self):
         return self.AveragedValue
     
-
 class AveragedUtilBaseWOInitValue(AveragedUtilBase):
-    def __init__(self, inAccpetNewValueFunc) -> None:
-        super().__init__(None)
-        self.AcceptNewValue = self.__AcceptInitValue
-        self.__AcceptNewValueFunc = inAccpetNewValueFunc
+    def __init__(self, inAcceptNewValueFunc) -> None:
+        super().__init__(None, self.__AcceptInitValue)
+        self.__AcceptNewValueFunc = inAcceptNewValueFunc
 
     def __AcceptInitValue(self, inInitValue):
         self.AveragedValue = inInitValue
         self.AcceptNewValue = self.__AcceptNewValueFunc
-        
+
 class EMAValue(AveragedUtilBaseWOInitValue):
     def __init__(self, inDecay) -> None:
         super().__init__(self.__EMAAcceptNewValue)
