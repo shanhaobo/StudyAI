2200776f34d3836359380f298cd770102be5f75e
shanhaobo <shanhaobo@gmail.com>
2023-07-09 21:21:42 +0800

    sth

--- KongMing/Utils/Delegate.py
+++ KongMing/Utils/Delegate.py
@@ -1,13 +1,13 @@
 class Delegate:
     def __init__(self):
-        self.functions = []
+        self.FunctionList = []
 
-    def __call__(self, inArgs, inKVArgs) -> None:
-        for func in self.functions:
-            func(inArgs, inKVArgs)
+    def __call__(self, *inArgs, **inKVArgs) -> None:
+        for tFunc in self.FunctionList:
+            tFunc(*inArgs, **inKVArgs)
 
     def add(self, func) -> None:
-        self.functions.append(func)
+        self.FunctionList.append(func)
 
     def remove(self, func) -> None:
-        self.functions.remove(func)
+        self.FunctionList.remove(func)
